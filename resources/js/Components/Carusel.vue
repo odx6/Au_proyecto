0750614b41e810b4527b52b2c54e73a875fb1717
Ever <you@example.com>
2025-01-23 15:16:22 -0600

    proyecto inciado

---
+++ resources/js/Components/Carusel.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { Carousel, Slide } from 'vue-carousel';
+ 
+ export default {
+   ...
+   components: {
+     Carousel,
+     Slide
+   }
+   ...
+ };
+
+
+</script>
+
+<template>
+    <carousel :per-page="1" :navigate-to="someLocalProperty" :mouse-drag="false">
+    <slide>
+      Slide 1 Content
+    </slide>
+    <slide>
+      Slide 2 Content
+    </slide>
+  </carousel>
+</template>
